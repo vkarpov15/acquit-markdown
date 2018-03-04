c425d5b9d8531d6938bc0319cd44eca3a7436f65
Valeri Karpov <val@karpov.io>
2018-03-04 09:13:08 -0800

    feat(bin): add -i option

--- bin/acquit-markdown.js
+++ bin/acquit-markdown.js
@@ -10,13 +10,14 @@ commander.
   option('-p, --path [path]', 'File/directory to use').
   option('-r, --require [plugin]', 'Require plugins').
   option('-h, --header [path]', 'Header file').
+  option('-i, --it [bool]', 'Whether to prepend "it"').
   parse(process.argv);
 
 if (!commander.path) {
   throw 'Need to set --path!';
 }
 
-require('../')();
+require('../')({ it: commander.it });
 
 if (commander.require) {
   const plugins = Array.isArray(commander.require) ? commander.require : [commander.require];
